4ae64eafb7d8b47a439d2a485d11f5e616cc9f76
gaolijun <15801419993@163.com>
2018-03-03 20:01:23 +0800

    会员

--- js/index.js
+++ js/index.js
@@ -4,13 +4,24 @@ new Vue({
   	inApp: true,
     http:"//yaoyakun-market.Yongche.org",
     //http:"//market.Yongche.com",
+    userInfo: {
+				userId:"",
+				level:0,
+				orders:0,
+				mileage:0
+			},
+		leftOrders:3,
+		leftMiles:4,
     currentLevel:0,
+    guide:'',
+    lineBar:'50%',
     showDetail:false,
     cards:[],
     rightsList:[],
-    userLevelRights:[]
+    userLevelRights:[],
+    userActives:[]
   },
-  mounted: function(){
+  created: function(){
   	this.initData();
   	
   },
@@ -24,8 +35,8 @@ new Vue({
 		    vm.cards = data.cards;
 		    vm.rightsList = data.rightsList;
 		    vm.userLevelRights = data.userLevelRights;
-		    
-		    
+		    vm.userActives = data.userActives;
+		    vm.getUserInfo();
 		    vm.initScroll();
 		    vm.$nextTick(function(){
 		    	vm.initSwiper();
@@ -48,11 +59,34 @@ new Vue({
 		    console.log(error);
 		  });*/
 		},
+		getUserInfo:function(){
+			this.userInfo = {
+				userId:"",
+				level:3,
+				orders:13,
+				mileage:15,
+				isSaveLevel:true
+			}
+			//判断是否保级
+			/*if(this.userInfo.isSaveLevel){
+					
+			}else{
+				
+			}*/
+			
+		
+				
+			if(this.userInfo.level<3){
+				this.guide = '差&nbsp;<span class="guideNum">'+this.leftOrders+'</span>&nbsp;单或&nbsp;<span class="guideNum">'+this.leftMiles+'</span>&nbsp;里程，升级到'+this.userLevelRights[this.userInfo.level+1].levelTitle+'&nbsp;&gt;'
+			}else{
+				this.guide = '您已经是最高级别享受贵宾权益'
+			}
+		},
   	initSwiper:function(){
   		var swiper = new Swiper('.swiper-container-h', {
-  			initialSlide:this.currentLevel,
+  			initialSlide:this.userInfo.level,
 	      slidesPerView: 2,
-	      spaceBetween: 24,
+	      spaceBetween: 12,
 	      centeredSlides: true,
 	      pagination: {
 	        el: '.swiper-pagination',
